167b15dd350617c6bbbb676c62ac257c53eb5722
yoshioka-s <yoshioka63@gmail.com>
2022-11-29 12:16:20 +0900

    feat: experiences

---
+++ src/pages/experiences/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+import { graphql, PageProps } from "gatsby"
+
+const BlogPostTemplate: React.FC = () =>  {
+  return (
+    <div>
+      <div>
+        <h1>shu</h1>
+      </div>
+    </div>
+  )
+}
+
+export default BlogPostTemplate
